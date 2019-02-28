c154bc4529711b49881475f35e475b78648e894d
Savannah Cook <savannahcook@Savannahs-MacBook-Pro.local>
2019-02-28 13:45:16 -0700

    Login changes

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -5,6 +5,102 @@ import { connect } from 'react-redux';
 import { updateUser } from './../../ducks/reducer'
 // import bootstrap from 'react-bootstrap'
 
+// const styles = {
+//     bg: {
+//         display: "flex",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         height: "-webkit-fill-available",
+//     },
+//     title: {
+//         fontSize: 40,
+//         // margin: 0,
+//         fontWeight: 700,
+//         fontFamily: 'sans-serif'
+//     },
+//     bgLayer: {
+//         background: '#404040',
+//         display: 'flex',
+//         padding: 0,
+//         height: 240 
+//     },
+//     bgLayerTitle: {
+//         color: '#f2f2f2',
+//         fontSize: 20,
+//         textAlign: 'left',
+//         marginTop: '9%',
+//         marginLeft: '9%',
+//         marginRight: '9%'
+//     },
+//     formGroup: {
+//         marginLeft: '9%',
+//         marginRight: '9%',
+//         marginBottom: '9%',
+//         paddingTop: 3,
+//         paddingBottom: 3
+//     },
+//     btn: {
+//         width: '100%',
+//         paddingTop: 3,
+//         paddingBottom: 3
+//     },
+//     checkBox: {
+//         textAlign: 'left',
+//         color: 'white',
+//         display: 'flex',
+//         paddingBottom: 16
+//     },
+//     textTitle: {
+//         color: '#f2f2f2',
+//         marginTop: '9%',
+//         marginLeft: '9%',
+//         marginRight: '9%'
+//     },
+//     formBtn: {
+//         marginLeft: '10%',
+//         marginRight: '10%',
+//         marginBottom: '10%'
+//     },
+//     facebookBtn: {
+//         width: '100%',
+//         background: '#4d79ff',
+//         border: 0,
+//         paddingTop: 5,
+//         paddingBottom: 5,
+//         marginBottom: 10,
+//         fontSize: 12
+//     },
+//     googleBtn: {
+//         width: '100%',
+//         background: '#ff1a1a',
+//         border: 0,
+//         paddingTop: 5,
+//         paddingBottom: 5,
+//         fontSize: 12,
+//         marginTop: 13
+//     },
+//     accountBtn: {
+//         width: '100%',
+//         background: '#0d0d0d',
+//         border: 0,
+//         paddingTop: 5,
+//         paddingBottom: 5,
+//         marginTop: 70,
+//         fontSize: 12
+//     },
+//     divide: {
+//         color: '#ffffff',
+//         paddingTop: 10,
+//         paddingBottom: 1
+//     },
+//     footer: {
+//         background: '#e7e7e7',
+//         textAlign: 'left',
+//     },
+//     footerTxt: {
+//         padding: 20
+//     }
+// }
 const styles = {
     bg: {
         display: "flex",
@@ -13,7 +109,7 @@ const styles = {
         height: "-webkit-fill-available",
     },
     title: {
-        fontSize: 40,
+        fontSize: 50,
         // margin: 0,
         fontWeight: 700,
         fontFamily: 'sans-serif'
@@ -21,12 +117,11 @@ const styles = {
     bgLayer: {
         background: '#404040',
         display: 'flex',
-        padding: 0,
-        height: 240 
+        padding: 0
     },
     bgLayerTitle: {
         color: '#f2f2f2',
-        fontSize: 20,
+        fontSize: 30,
         textAlign: 'left',
         marginTop: '9%',
         marginLeft: '9%',
@@ -35,14 +130,12 @@ const styles = {
     formGroup: {
         marginLeft: '9%',
         marginRight: '9%',
-        marginBottom: '9%',
-        paddingTop: 3,
-        paddingBottom: 3
+        marginBottom: '9%'
     },
     btn: {
         width: '100%',
-        paddingTop: 3,
-        paddingBottom: 3
+        paddingTop: 10,
+        paddingBottom: 10
     },
     checkBox: {
         textAlign: 'left',
@@ -65,28 +158,23 @@ const styles = {
         width: '100%',
         background: '#4d79ff',
         border: 0,
-        paddingTop: 5,
-        paddingBottom: 5,
-        marginBottom: 10,
-        fontSize: 12
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginBottom: 10
     },
     googleBtn: {
         width: '100%',
         background: '#ff1a1a',
         border: 0,
-        paddingTop: 5,
-        paddingBottom: 5,
-        fontSize: 12,
-        marginTop: 13
+        paddingTop: 10,
+        paddingBottom: 10
     },
     accountBtn: {
         width: '100%',
         background: '#0d0d0d',
         border: 0,
-        paddingTop: 5,
-        paddingBottom: 5,
-        marginTop: 70,
-        fontSize: 12
+        paddingTop: 10,
+        paddingBottom: 10
     },
     divide: {
         color: '#ffffff',
@@ -101,6 +189,7 @@ const styles = {
         padding: 20
     }
 }
+
 
 
 class Auth extends Component {
@@ -150,6 +239,46 @@ class Auth extends Component {
     }
     render() {
         return (
+            // <div className="row">
+            //     <div className="col-sm-12" style={styles.bg}>
+            //         <div className="col-sm-3"></div>
+            //         <div className="col-sm-6 mobile" style={{textAlign: 'center'}}>
+            //             <div style={{display: 'block'}}>
+            //                 <p className="title" style={styles.title}>CARDKLOUT</p>
+            //             </div>
+            //             <div className="col-sm-12 mobileForm" style={styles.bgLayer}>
+            //                 <div className="col-sm-6 form1" style={{borderRight: '1px solid #ffffff'}}>
+            //                     <p style={styles.bgLayerTitle}>Login</p>
+            //                     <form style={styles.formGroup}>
+            //                         <div className="form-group">
+            //                             <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="USERNAME/EMAIL" style={{height: '5vh'}} onChange={e => this.handleChange("username", e.target.value)}/>
+            //                         </div>
+            //                         <div className="form-group">
+            //                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="PASSWORD" style={{height: '5vh'}} onChange={e => this.handleChange("password", e.target.value)}/>
+            //                         </div>
+            //                         <div className="form-check" style={styles.checkBox}>
+            //                             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            //                             <label style={{fontSize: 12}} className="form-check-label" htmlFor="exampleCheck1">keep me signed in</label>
+            //                         </div>
+            //                         <button type="button" style={styles.btn} className="btn btn-primary" onClick={this.login}>LOG IN</button>
+            //                     </form>
+            //                 </div>
+            //                 <div className="col-sm-6 form2">
+            //                     <form style={styles.formBtn}>
+            //                         <button type="button" style={styles.accountBtn} className="btn btn-primary">REQUEST ACCOUNT</button>
+            //                         <label style={styles.divide}>OR</label>
+            //                         <button type="button" style={styles.facebookBtn} className="btn btn-primary">REQUEST WITH FACEBOOK</button>
+            //                         <button type="button" style={styles.googleBtn} rsclassName="btn btn-primary">REQUEST WITH GOOGLE</button>
+            //                     </form>
+            //                 </div>
+            //             </div>
+            //             <div className="col-sm-12" style={styles.footer}>
+            //                 <p style={styles.footerTxt}>Can't get in to your account? Did you <a href="#" style={{color: '#000000', textDecoration: 'underline'}}>forget your password?</a></p>
+            //             </div>
+            //         </div>
+            //         <div className="col-sm-3"></div>
+            //     </div>
+            // </div>
             <div className="row">
                 <div className="col-sm-12" style={styles.bg}>
                     <div className="col-sm-3"></div>
@@ -162,24 +291,28 @@ class Auth extends Component {
                                 <p style={styles.bgLayerTitle}>Login</p>
                                 <form style={styles.formGroup}>
                                     <div className="form-group">
-                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="USERNAME/EMAIL" style={{height: '5vh'}} onChange={e => this.handleChange("username", e.target.value)}/>
+                                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="USERNAME/EMAIL" />
                                     </div>
                                     <div className="form-group">
-                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="PASSWORD" style={{height: '5vh'}} onChange={e => this.handleChange("password", e.target.value)}/>
+                                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="PASSWORD" />
                                     </div>
                                     <div className="form-check" style={styles.checkBox}>
                                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                                         <label style={{fontSize: 12}} className="form-check-label" htmlFor="exampleCheck1">keep me signed in</label>
                                     </div>
-                                    <button type="button" style={styles.btn} className="btn btn-primary" onClick={this.login}>LOG IN</button>
+                                    <button style={styles.btn} type="button" className="btn btn-primary">LOG IN</button>
                                 </form>
                             </div>
                             <div className="col-sm-6 form2">
+                                <p style={styles.textTitle}>
+                                    If you don't already have an account click<br/>
+                                    the button below to request an account
+                                </p>
                                 <form style={styles.formBtn}>
-                                    <button type="button" style={styles.accountBtn} className="btn btn-primary">REQUEST ACCOUNT</button>
+                                    <button style={styles.accountBtn} type="button" className="btn btn-primary">REQUEST ACCOUNT</button>
                                     <label style={styles.divide}>OR</label>
-                                    <button type="button" style={styles.facebookBtn} className="btn btn-primary">REQUEST WITH FACEBOOK</button>
-                                    <button type="button" style={styles.googleBtn} rsclassName="btn btn-primary">REQUEST WITH GOOGLE</button>
+                                    <button style={styles.facebookBtn} type="button" className="btn btn-primary">REQUEST WITH FACEBOOK</button>
+                                    <button style={styles.googleBtn} type="button" className="btn btn-primary">REQUEST WITH GOOGLE</button>
                                 </form>
                             </div>
                         </div>
